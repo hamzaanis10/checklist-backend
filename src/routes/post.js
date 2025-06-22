b9dd21e44f215c8e47aea20ad100cec454cdf805
Hamza\Hamza10 <hamza.anis@cloudasset.com>
2025-06-22 05:07:17 +0500

    edit endpoint

--- src/routes/post.js
+++ src/routes/post.js
@@ -69,6 +69,28 @@ router.get("/:id", protect, async (req, res) => {
       res.status(500).json({ message: err.message });
     }
   });
+
+// Edit a post
+router.put("/:id", protect, async (req, res) => {
+    const { title, content } = req.body;
+  
+    try {
+      const post = await Post.findById(req.params.id);
+  
+      if (!post) {
+        return res.status(404).json({ message: "Post not found" });
+      }
+      post.title = title || post.title;
+      post.content = content || post.content;
+  
+      await post.save();
+  
+      res.status(200).json(post);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  });
+  
   
 
 module.exports = router;
